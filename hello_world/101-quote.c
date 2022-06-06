03c23c3d869dad39296bca4666ddfa64e9a59100
Gotmished <5002@holbertonschool.com>
2022-06-06 00:04:06 -0700

    Initial commit

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19"
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
