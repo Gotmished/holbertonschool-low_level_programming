805df43c22024dfdf3b4463b71427d7654953de2
Gotmished <5002@holbertonschool.com>
2022-06-07 00:46:35 -0700

    Initial commit for task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints the digits of base 16,
+ * (hexadecimal) in lowercase, followed by a new line
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 0; n < 10; n = n + 1)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch = ch + 1)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
