195b1b1bd27262386c7e5348a2085250dd34e276
Gotmished <5002@holbertonschool.com>
2022-06-06 22:47:48 -0700

    Initial commit for task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints the alphabet in lowercase,
+ * followed by a new line. Letters q and e are excluded.
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+		while (ch <= 'z')
+		{
+			if (ch != 'e' && ch != 'q')
+		}
+			putchar(ch);
+		}
+ch = ch + 1;
+		}
+putchar('\n');
+return (0);
+}
