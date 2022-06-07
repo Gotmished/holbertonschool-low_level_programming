fffa8e62883aad4126231ceaf35be64c271dd0de
Gotmished <5002@holbertonschool.com>
2022-06-06 23:52:43 -0700

    Initial commit for task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints the alphabet in lowercase
+ * and reverse, followed by a new line
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch = ch + 1)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
