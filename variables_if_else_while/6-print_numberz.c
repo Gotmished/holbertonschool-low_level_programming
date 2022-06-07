dff8c6ff396aaec49d866db2f97a027532c9303e
Gotmished <5002@holbertonschool.com>
2022-06-06 23:21:05 -0700

    Initial commit for task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints all single-digit numbers
+ * of base 10 beginning at 0, followed by a new line.
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char nu;
+
+	for (nu = '0'; nu <= '9'; nu = nu + 1)
+	{
+		putchar(nu);
+	}
+	putchar('\n');
+	return (0);
+}
