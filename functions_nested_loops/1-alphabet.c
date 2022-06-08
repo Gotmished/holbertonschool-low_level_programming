e0c62149fe15799b056775e302a8235e08b8c167
Gotmished <5002@holbertonschool.com>
2022-06-07 20:16:38 -0700

    Updated description in script for task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - this function prints the alphabet
+ * print_alphabet - this function prints the alphabet
  * in lowercase, followed by a new line
  */
 
 void print_alphabet(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch = ch + 1)
 	{
 		_putchar(ch);
