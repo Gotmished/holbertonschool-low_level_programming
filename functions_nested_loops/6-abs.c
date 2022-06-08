2df484469ed77cf2e67a55e2bc6c8ea0b517853e
Gotmished <5002@holbertonschool.com>
2022-06-07 23:10:12 -0700

    Updated task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,15 +3,13 @@
 /**
  * _abs - computes the absolute value
  * of an integer
- * @int: the integer from which the absolute
+ * @n: the integer from which the absolute
  * value is to be found
  * Return: the absolute value of int
  */
 
-int _abs(int)
+int _abs(int n)
 {
-	int n;
-
 	if (n >= 0)
 	{
 		return (n);
