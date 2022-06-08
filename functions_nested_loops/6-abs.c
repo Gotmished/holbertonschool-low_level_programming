b2bfc0b7f911eae72aad0291893e70e554b49b2a
Gotmished <5002@holbertonschool.com>
2022-06-07 23:07:30 -0700

    Initial commit for task 6; updated header file

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ * of an integer
+ * @int: the integer from which the absolute
+ * value is to be found
+ * Return: the absolute value of int
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
