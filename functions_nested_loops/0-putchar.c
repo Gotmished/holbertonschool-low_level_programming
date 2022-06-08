2792e4bce5369d8ca6132404b2c86e3c576e06b2
Gotmished <5002@holbertonschool.com>
2022-06-07 19:29:00 -0700

    Adding new directory, readme, and task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints '_putchar'
+ * followed by a new line
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme for project 3, week 3
