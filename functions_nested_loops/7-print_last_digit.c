e6085bc194df0e38543cbf413c2d66da5c4230c1
Gotmished <5002@holbertonschool.com>
2022-06-07 23:30:44 -0700

    Initial commit task 7; updated header file

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns the value of
+ * the last digit
+ * @n: the integer from which the last
+ * digit is obtained
+ * Return: the last digit of n
+ */
+
+int print_last_digit(int n);
+{
+	int a;
+
+	a = n % 10
+	if (n < 0)
+	{
+		n = -n;
+	}
+	return (a);
+}
