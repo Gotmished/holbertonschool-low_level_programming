55da663f9bd523950261b6ee4c4f43de8b46e582
Gotmished <5002@holbertonschool.com>
2022-06-09 02:28:50 -0700

    Updated task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,14 +16,12 @@ void times_table(void)
 			if (k < 10 && j == 9)
 			{
 				_putchar(k + '0');
-				_putchar('$');
 			}
 
 			else if (k >= 10 && j == 9)
 			{
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-				_putchar('$');
 			}
 
 			if (k < 10 && j != 9)
