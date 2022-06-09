a80fc8c03ccba65a28e4326be7eb282d019ee32b
Gotmished <5002@holbertonschool.com>
2022-06-09 04:35:07 -0700

    Initial commit task 10; updated header file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: integer 1
+ * @b: integer 2
+ * Return: result of adding a and b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
