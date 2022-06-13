bf71c844d9f7608ed72958e0e16a4fd6249915f6
Gotmished <5002@holbertonschool.com>
2022-06-12 22:42:06 -0700

    Updated task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - with the exception of 2 and 4
+ * prints numbers 0 to 9
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+		i = i + 1;
+	}
+	_putchar('\n');
+}
