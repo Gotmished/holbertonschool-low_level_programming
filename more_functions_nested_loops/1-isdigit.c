a7f5bab9ba11fee833edeea527ecd61beb3ba112
Gotmished <5002@holbertonschool.com>
2022-06-12 21:51:15 -0700

    Re-wrote task one

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit between 0 and 9
- * @c: int to be checked
+ * _isdigit - checks for a digit between 1 and 9
+ * @c: the integer to check
  * Return: 1 if c is a digit, 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+	int i, n;
+
+	i = 0;
+	n = 0;
+	while (i <= '9')
+	{
+		if (c == i)
+		{
+			n = 1;
+		}
+		i = i + 1;
+	}
+	return (n);
 }
