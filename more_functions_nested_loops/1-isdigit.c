89fa5121e10c3be9d4b863f919e50a9094b4b371
Gotmished <5002@holbertonschool.com>
2022-06-12 21:30:53 -0700

    Initial commit task one

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit between 1 and 9
+ * @c: the integer to check
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	int i, n;
+
+	i = 0;
+	n = 0;
+	while (i <= '9')
+	{
+		if (c == i)
+		{
+			n = 1;
+		}
+		i = i + 1;
+	}
+	return (n);
+}
