af2d6ad4512c98fe6b95499e29a37f4d38037afe
Gotmished <5002@holbertonschool.com>
2022-06-12 22:01:39 -0700

    Initial commit task two

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: the result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	int i;
+
+	i = a * b;
+	return (i);
+}
