320d98f04de81c721b24b09b5392ebdc871513a1
Gotmished <5002@holbertonschool.com>
2022-06-15 00:04:06 -0700

    Initial commit task seven; updated header file

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of
+ * a string, followed by a new line
+ * @str: the string in question
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i = i + 1;
+	}
+
+	if (i % 2 == 0)
+	{
+		j = i / 2;
+		while (j < i)
+		{
+			_putchar(str[j]);
+			j = j + 1;
+		}
+	}
+	else
+	{
+		j = (i - 1) / 2;
+		while (j < i)
+		{
+			_putchar(str[j]);
+			j = j + 1;
+		}
+	}
+	_putchar('\n');
+}
