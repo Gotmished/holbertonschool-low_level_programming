c00dbb303993cc62da3bca4bc4e80f2295613247
Gotmished <5002@holbertonschool.com>
2022-06-15 00:49:04 -0700

    Initial commit task nine; updated header file

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies a string to a buffer
+ * @dest: points to buffer location
+ * @src: points to string location
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, length;
+
+	length = 0;
+	while (src[length] <= '\0')
+	{
+		length = length + 1;
+	}
+
+	i = 0;
+	while (i < length)
+	{
+		dest[i] = src[i];
+		i = i + 1;
+	}
+	return (dest);
+}
