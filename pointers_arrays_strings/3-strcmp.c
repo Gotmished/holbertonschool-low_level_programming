954a2adc53b1c187758299d363cebe9e78d5f67d
Gotmished <5002@holbertonschool.com>
2022-06-16 02:39:30 -0700

    Initial commit task three; updated header file

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: negative int if s1 < s2
+ * 0 if s1 length = s2 length
+ * positive int if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i = i + 1;
+	}
+
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		j = j + 1;
+	}
+	return (i - j);
+}
