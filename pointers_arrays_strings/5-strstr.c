f751e99b96ad18adf7b81d0c7463f3d7b01f3ac2
Gotmished <5002@holbertonschool.com>
2022-06-17 05:19:16 -0700

    Initial commit task five; updated header file

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a substring
+ * @haystack: the string to be searched
+ * @needle: the substring to be found
+ * Return:  a pointer to the located substring
+ * NULL otherwise
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0')
+		{
+			if (haystack[i + j] == needle[j])
+			{
+				j = j + 1;
+			}
+			else
+			{
+				break;
+			}
+		}
+	        if (needle[j] != '\0')
+		{
+			i = i + 1;
+			j = 0;
+		}
+		else
+		{
+			return (&haystack[i]);
+		}
+	}
+	return (0);
+}
