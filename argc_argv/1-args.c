56db671c257186634c0ba2b6a42f89f8f3bf4a55
Gotmished <5002@holbertonschool.com>
2022-06-21 19:59:41 -0700

    Initial commit task one

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the program's own name, followed by a new line
+ * @argc: the number of arguments
+ * @argv: array of arguments
+ * Return: 0 (success)
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
