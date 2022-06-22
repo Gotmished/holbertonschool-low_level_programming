ebfac438f8f3f70c816eb528844243559dd90f7d
Gotmished <5002@holbertonschool.com>
2022-06-22 01:54:52 -0700

    Initial commit task three

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: 0 (success), 1 (error)
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b, c;
+
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		c = a * b;
+		printf("%d\n", c);
+		return (0);
+	}
+}
