3f4a15749baaa6691739b2484ab8e6b7a5361e68
Gotmished <5002@holbertonschool.com>
2022-06-23 03:47:53 -0700

    Malloc update for NULL cases

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,25 +11,29 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *a;
-	unsigned int i, j, length;
+	char *a, *b, *c;
+	unsigned int i, j;
 
 	if (s1 == NULL)
 	{
 		i = 0;
+		b = malloc(1);
 	}
 	else
 	{
 		i = strlen(s1);
+		b = s1;
 	}
 
 	if (s2 == NULL)
 	{
 		j = 0;
+		c = malloc(1);
 	}
 	else
 	{
 		j = strlen(s2);
+		c = s2;
 	}
 
 	a = malloc(sizeof(char) * (i + j) + 1);
@@ -37,19 +41,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-
-	length = 0;
-	while (length < i)
-	{
-		a[length] = s1[length];
-		length = length + 1;
-	}
-	length = 0;
-	while (length < j)
-	{
-		a[length + i] = s2[length];
-		length = length + 1;
-	}
-	a[i + j] = '\0';
+	strcpy(a, b);
+	strcat(a, c);
 	return (a);
 }
