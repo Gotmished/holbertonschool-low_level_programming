c24743c13f3497050ec4a6b62280ad64f1c10c7a
Gotmished <5002@holbertonschool.com>
2022-06-23 00:32:30 -0700

    Initial commit task two; updated header file

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: NULL on failure, the concatenated result otherwise
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	unsigned int i, j;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i = i + 1;
+	}
+
+	j = strlen(s2);
+
+	a = malloc(sizeof(char) * (i + j) + 1);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(a, s1);
+	strcat(a, s2);
+	return (a);
+}
