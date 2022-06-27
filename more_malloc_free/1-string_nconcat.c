78ae75c712aa82d12e6c22f53f249b7161b71de7
Gotmished <5002@holbertonschool.com>
2022-06-27 01:40:02 -0700

    Updated string length counts

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		i = 0;
-		while (s1[i] <= '\0')
+		while (s1[i])
 		{
 			i = i + 1;
 		}
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		j = 0;
-		while (s2[j] <= '\0')
+		while (s2[j])
 		{
 			j = j + 1;
 		}
