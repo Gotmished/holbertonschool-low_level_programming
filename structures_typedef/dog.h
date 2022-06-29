71cd12e9d9fdeaebfaafa567f9b466fb41757374
Gotmished <5002@holbertonschool.com>
2022-06-29 00:37:28 -0700

    Initial commit task zero; added directory and readme file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about a dog
+ * @name: the dog's name
+ * @owner: the dog's owner
+ * @age: the dog's age
+ */
+
+struct dog
+{
+	char *name, *owner;
+	float age;
+};
+
+#endif
