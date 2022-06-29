c8ce97ff846e5a336412c5d85a26a817b73b4d88
Gotmished <5002@holbertonschool.com>
2022-06-29 03:19:57 -0700

    Initial commit task five; updated header file

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees all allocated memory
+ * @d: struct dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
