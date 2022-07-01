bd37a7f964967eba9dda07f62a438a4a32b5fcbb
Gotmished <5002@holbertonschool.com>
2022-07-01 03:48:17 -0700

    Initial commit task four

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints opcodes of own main function
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	char *s;
+	int a, i;
+	/* lone argument is number of bytes */
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	a = atoi(argv[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	s = (char *)main;
+	i = 0;
+	while (i < a)
+	{
+		printf("%.2hhx ", s[i]);
+		if (i == a - 1)
+		{
+			printf("%.2hhx\n", s[i]);
+			break;
+		}
+		i = i + 1;
+	}
+	return (0);
+}
