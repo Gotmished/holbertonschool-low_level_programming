00aaefc112d3e10800c45f5602b617e7470d87eb
Gotmished <5002@holbertonschool.com>
2022-07-12 05:22:49 -0700

    Initial commit task three; updated header file

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to a pointer to head of linked list
+ * @str: string to be assigned to the node
+ * Return: the address of the new element, or NULL upon failure
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *last_node;
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	/* new node needs to be associated with given data */
+	/* duplicate, as *str is const */
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	/* new node points to end of list */
+	new_node->next = NULL;
+	/* new node is now the head node if head points to NULL */
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	/* Otherwise, find and set last node to new node */
+	else
+	{
+		/* keep setting last node's address to next until it is NULL */
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+	}
+	last_node->next = new_node;
+	return (new_node);
+}
