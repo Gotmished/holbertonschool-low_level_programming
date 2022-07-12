7f36ab6888899ac9dea5e94006274332a20282b2
Gotmished <5002@holbertonschool.com>
2022-07-12 05:36:08 -0700

    Fixed freeing and output

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,6 +3,24 @@
 #include "lists.h"
 
 /**
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i = i + 1;
+	}
+	return (i);
+}
+
+/**
  * add_node_end - adds a new node at the end of a list_t list
  * @head: pointer to a pointer to head of linked list
  * @str: string to be assigned to the node
@@ -14,14 +32,19 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node, *last_node;
 
 	new_node = malloc(sizeof(*new_node));
-	if (new_node == NULL)
+	if (new_node == NULL || str == NULL)
 	{
 		return (NULL);
 	}
 	/* new node needs to be associated with given data */
 	/* duplicate, as *str is const */
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = _strlen(new_node->str);
 	/* new node points to end of list */
 	new_node->next = NULL;
 	/* new node is now the head node if head points to NULL */
@@ -32,6 +55,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	/* Otherwise, find and set last node to new node */
 	else
 	{
+		last_node = *head;
 		/* keep setting last node's address to next until it is NULL */
 		while (last_node->next != NULL)
 		{
