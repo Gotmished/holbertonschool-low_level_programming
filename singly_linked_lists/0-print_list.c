d57095cd3614a3fec6ab06876c6d21c28d6f3550
Gotmished <5002@holbertonschool.com>
2022-07-12 01:30:26 -0700

    Initial commit task zero; added readme and header file

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all of the elements of a list_t list
+ * @h: pointer to list_t list to print
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int number;
+
+	number = 0;
+	/* as long as the pointer is pointing to something extant */
+	while (h != NULL)
+	{
+		/* if the pointer is pointing to a NULL list */
+		if (h->str ==  NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			/* print len and str, before making h point to next */
+			/*
+			 * next is a pointer to struct list_s,
+			 * which is the struct containing str and len
+			 */
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		number = number + 1;
+	}
+	return (number);
+}
