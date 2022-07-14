d3adc5b0c095c2bd0f24b9fff470dc640ec29081
Gotmished <5002@holbertonschool.com>
2022-07-14 04:38:26 -0700

    Updated code for efficiency

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -12,19 +12,9 @@ void free_list(list_t *head)
 
 	while (head != NULL)
 	{
-		temporary = head->next;
-		free(head->str);
-		free(head);
-		head = temporary;
-		/*
-		 * Head currently points to the first node, so
-		 * let temp point to the next node, then free head's prior
-		 * association with str (because memory was allocated for
-		 * the string (it was duplicated into allocated memory)
-		 * and *str is a pointer to it. Not the case
-		 * for the int len), and then head itself, before
-		 * allowing head to take over from temp, and
-		 * point to the next node.
-		 */
+		temporary = head;
+		head = head->next;
+		free(temporary->str);
+		free(temporary);
 	}
 }
