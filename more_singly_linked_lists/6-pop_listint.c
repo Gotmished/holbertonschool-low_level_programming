fc5d3a7d576b0e093462b7be8fa501fa96e60423
Gotmished <5002@holbertonschool.com>
2022-07-14 03:50:33 -0700

    Initial commit task six; updated header file

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t list
+ * @head: a pointer to a pointer to the head of the list
+ * Return: n, the head node's data, or 0 if the list is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temporary;
+	int n;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	if (*head != NULL)
+	{
+		temporary = *head;
+		*head = (*head)->next;
+		n = temporary->n;
+		free(temporary);
+	}
+	return (n);
+}
