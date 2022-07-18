b503eeec869c7c3a4c669c28a49d7420a647a056
Gotmished <5002@holbertonschool.com>
2022-07-18 04:44:03 -0700

    Initial commit task five; updated header file

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * count_bits - a counting function to feed into flip_bits
+ * @a: n ^ m from below
+ * Return: a count to be sent below
+ */
+
+int count_bits(int a)
+{
+	int count;
+
+	count = 0;
+	while (a > 0)
+	{
+		a = a & (a - 1);
+		count = count + 1;
+	}
+	return (count);
+}
+
+/**
+ * flip_bits - returns the number of bits needed to flip
+ * to get from one number to another
+ * @n: the number in which bits are to be flipped
+ * @m: the reference number
+ * Return: the number of bits needed
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (count_bits(n ^ m));
+}
