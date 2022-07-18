6f8e1929eb5fd558f940bf29c5a838aaac555d8e
Gotmished <5002@holbertonschool.com>
2022-07-18 02:06:34 -0700

    Added explanatory comments

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -11,6 +11,15 @@ void print_binary(unsigned long int n)
 	if (n > 1)
 	{
 		print_binary(n >> 1);
+		/*
+		 * eg (98) 1100010 becomes
+		 *         0110001 and is passed back to the function
+		 */
 	}
 	_putchar((n & 1) + '0');
+	/*
+	 * Prints result of 1100010
+	 *                & 0000001
+	 *                =       0
+	 */
 }
