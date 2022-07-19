b2b74b0458b41ffe82d3d01afb6abd65926d3978
Gotmished <5002@holbertonschool.com>
2022-07-19 04:06:26 -0700

    Added O_TRUNC flag

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -36,12 +36,14 @@ int create_file(const char *filename, char *text_content)
 	int file;
 	ssize_t writing;
 
-	file = open(filename, O_CREAT | O_RDWR, 0600);
+	/* TRUNC clears a pre-existing file to overwrite */
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (filename == NULL || file == -1)
 	{
 		return (-1);
 	}
 
+	/* Replace with empty string */
 	if (text_content == NULL)
 	{
 		text_content = "";
