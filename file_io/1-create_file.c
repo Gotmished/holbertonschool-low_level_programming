0350ec390a17f1e1a4c75a5e828bd70c88edc57f
Gotmished <5002@holbertonschool.com>
2022-07-19 03:55:43 -0700

    Initial commit task one; updated header file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,57 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ *
+ * Return: length of the string
+ */
+
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i = i + 1;
+	}
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: the text file to be created
+ * @text_context: a NULL-terminated string to be written
+ * Return: 1 upon success, -1 upon failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t writing;
+
+	file = open(filename, O_CREAT | O_RDWR, 0600);
+	if (filename == NULL || file == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	writing = write(file, text_content, _strlen(text_content));
+	if (writing == -1)
+	{
+		return (-1);
+	}
+
+	return (1);
+}
