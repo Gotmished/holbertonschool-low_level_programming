60424e8f6f8afc23e9a4dd3641003d80d8c9b76e
Gotmished <5002@holbertonschool.com>
2022-07-22 06:32:45 -0700

    Edited NULL conditions

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,12 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *temp;
 	unsigned int i;
 
-	if (*h == NULL && idx > 0)
-	{
-		return (NULL);
-	}
 	new_node = malloc(sizeof(*new_node));
-	if (new_node == NULL)
+	if (new_node == NULL || *h == NULL)
 	{
 		return (NULL);
 	}
