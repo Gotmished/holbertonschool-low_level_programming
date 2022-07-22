bd6b8121fc25c1bbcf9603bc6a2ab01f970b9388
Gotmished <5002@holbertonschool.com>
2022-07-21 20:36:24 -0700

    Initial commit task three; updated header file

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i = i + 1;
+	}
+	return (i);
+}
+
+/**
+ * add_dnodeint_end - adds a new node at the end
+ * of a dlistint_t list
+ * @head: a pointer to a pointer to the first element
+ * @n: data to be stored in new node
+ * Return: the address of the new element,
+ * or NULL upon failure
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *last_node;
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+
+	}
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+	}
+	last_node->next = new_node;
+	new_node->prev = last_node;
+	return (new_node);
+}
