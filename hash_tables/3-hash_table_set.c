494d51683e8e95cdca60232555af02c019f9bf38
Gotmished <5002@holbertonschool.com>
2022-08-18 05:16:44 -0700

    Re-worked solution

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -2,6 +2,41 @@
 #include <string.h>
 #include "hash_tables.h"
 
+
+/**
+ * create_node - creates a new hash table element
+ * @key: the key relating to the node
+ * @value: the value associate with the node
+ *
+ * Return: the new node, or NULL on failure
+ */
+
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(*node));
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
 /**
  * hash_table_set - adds an element to a hash table
  * @ht: the hash table
@@ -12,35 +47,35 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, *head;
+	unsigned long int index;
+	hash_node_t *node;
 	hash_node_t **array;
-	unsigned long int index, size;
 
-	node = malloc(sizeof(*node));
-	if (node == NULL)
-	{
+	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
-	}
-
+	index = key_index((const unsigned char *)key, ht->size);
 	array = ht->array;
-	size = ht->size;
-	index = key_index((const unsigned char *)key, size);
-
-	node->key = strdup(key);
-	node->value = strdup(value);
-	node->next = NULL;
+	while (array[index] != NULL)
+	{
+		if (strcmp(array[index]->key, key) == 0)
+		{
+			if (array[index]->value == NULL)
+				return (0);
+			free(array[index]->value);
+			array[index]->value = strdup(value);
+			return (1);
+		}
+		array[index] = array[index]->next;
+	}
+	node = create_node(key, value);
 	if (array[index] == NULL)
 	{
 		array[index] = node;
 	}
 	else
 	{
-		head = array[index];
-		while (head->next != NULL)
-		{
-			head = head->next;
-		}
-		head->next = node;
+		node->next = array[index];
+		array[index] = node;
 	}
 	return (1);
 }
