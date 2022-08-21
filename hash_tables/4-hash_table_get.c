ede9aa6a47414d2965741074b67dc39a5cea55a9
Gotmished <5002@holbertonschool.com>
2022-08-21 02:05:02 -0700

    Initial commit task four; updated header file

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table
+ * @key: a non-empty string
+ * Return: the value associated with the element,
+ * or NULL if the key could not be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t **array;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+	{
+		return (NULL);
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+	array = ht->array;
+	while (array[index] != NULL)
+	{
+		if (strcmp(array[index]->key, key) == 0)
+		{
+			return (array[index]->value);
+		}
+		index = index + 1;
+	}
+	return (NULL);
+}
